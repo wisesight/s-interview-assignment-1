8ea9b1bdbf6ae8813c2f340bd15d5a7a29f42609
BumBimz.KS <l3uml3imz@gmail.com>
2022-08-26 12:46:41 +0700

    [+] Add struct

--- main.go
+++ main.go
@@ -6,11 +6,47 @@ import (
 	"time"
 )
 
+type Channel struct {
+	ChannelId       string `json:"id"`
+	Title           string `json:"snippet.title"`
+	Description     string `json:"snippet.description"`
+	SubscriberCount int    `json:"statistics.subscriberCount"`
+}
+
+type TrendingVideoResult struct {
+	Items         []TrendingVideo `json:"items"`
+	NextPageToken string          `json:"nextPageToken"`
+	PageInfo      PageInfo        `json:"pageInfo"`
+}
+
+type TrendingVideo struct {
+	ChannelId            string `json:"snippet.channelId"`
+	Title                string `json:"snippet.title"`
+	Description          string `json:"snippet.description"`
+	ChannelTitle         string `json:"snippet.channelTitle"`
+	DefaultLanguage      string `json:"snippet.defaultLanguage"`
+	DefaultAudioLanguage string `json:"snippet.defaultAudioLanguage"`
+}
+
+type PageInfo struct {
+	TotalResults   int `json:"totalResults"`
+	ResultsPerPage int `json:"resultsPerPage"`
+}
+
 func checkAccountExist(accountID string) bool {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Float32() < 0.5
 }
 
+func getNewSource() []Channel {
+	return []Channel{}
+}
+
 func main() {
-	fmt.Println("Hi K.Prem")
+	newChannels := getNewSource()
+	fmt.Println("=== New Channels ===")
+	for _, channel := range newChannels {
+		fmt.Printf("Title: %s \nsubscriberCount: %d\n\n", channel.Title, channel.SubscriberCount)
+	}
+	fmt.Println("=== End ===")
 }
